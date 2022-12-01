ec6f27943626da2fd9402134c86c6cae2fa30630
siwertzm <siwertzm@gmail.com>
2022-12-01 17:12:33 +0100

    home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rick_morty_front';
+
+  nbrPers: any;
+  nbrEp: any;
+  nbrLieu: any;
+
+  constructor(private Http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.getNbrEp();
+    this.getNbrLieu();
+    this.getNbrPers();
+  }
+
+  getNbrPers(){
+    this.Http.get('https://rickandmortyapi.com/api/character').subscribe({
+      next: (data) => { this.nbrPers = data;},
+      error: (err) => { console.log(err) }
+    })
+  }
+
+  getNbrEp(){
+    this.Http.get('https://rickandmortyapi.com/api/episode').subscribe({
+      next: (data) => { this.nbrEp = data;},
+      error: (err) => { console.log(err) }
+    })
+  }
+
+  getNbrLieu(){
+    this.Http.get('https://rickandmortyapi.com/api/location').subscribe({
+      next: (data) => { this.nbrLieu = data;},
+      error: (err) => { console.log(err) }
+    })
+  }
 }
